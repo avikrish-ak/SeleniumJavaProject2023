8e18bc85782b8806edd343c0e0a38e94abe2000d
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-02-23 09:51:00 +0530

    day05

---
+++ src/test/java/day05/Program04.java
@@ -0,0 +1,32 @@
+package day05;
+
+import org.testng.annotations.Test;
+
+public class Program04 {
+	
+	
+	@Test
+	public void testCase02()
+	{
+		System.out.println("Running test2..!");
+	}
+	
+	@Test
+	public void testCase01()
+	{
+		System.out.println("Running test1..!");
+	}
+	
+	@Test
+	public void testCase03()
+	{
+		System.out.println("Running test3..!");
+	}
+	@Test
+	public void testCase04()
+	{
+		System.out.println("Running test4..!");
+	}
+	
+
+}
